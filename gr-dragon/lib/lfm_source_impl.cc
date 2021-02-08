0451992a694fbd943bb831b959d2bb3cf17ef8a8
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2021-02-08 10:33:18 -0600

    Added a stream LFM source

---
+++ gr-dragon/lib/lfm_source_impl.cc
@@ -0,0 +1,122 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2021 gr-dragon author.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "lfm_source_impl.h"
+#include <dragon/vector-utils/vector_functions.h>
+
+namespace gr {
+namespace dragon {
+
+lfm_source::sptr
+lfm_source::make(float bandwidth,
+    float sweep_time,
+    float samp_rate,
+    float prf,
+    const std::vector<tag_t> &tags) {
+  return gnuradio::get_initial_sptr
+      (new lfm_source_impl(bandwidth, sweep_time, samp_rate, prf, tags));
+}
+
+/*
+ * The private constructor
+ */
+lfm_source_impl::lfm_source_impl(float bandwidth,
+    float sweep_time,
+    float samp_rate,
+    float prf,
+    const std::vector<tag_t> &tags)
+    : gr::sync_block("lfm_source",
+    gr::io_signature::make(0, 0, 0),
+    gr::io_signature::make(1, 1, std::round(sizeof(gr_complex)*samp_rate/prf))),
+    // Initialize the waveform parameters
+      bandwidth(bandwidth),
+      sweep_time(sweep_time),
+      samp_rate(samp_rate),
+      prf(prf),
+      tags(tags),
+      pri(1 / prf),
+      pulse_ctr(0) {
+  // Create the waveform from the above parameters
+  generateWaveform();
+  // Decide if the user wants tags
+  if (tags.empty())
+    set_tags = false;
+  else
+    set_tags = true;
+
+}
+
+/*
+ * Our virtual destructor.
+ */
+lfm_source_impl::~lfm_source_impl() = default;
+
+int
+lfm_source_impl::work(int noutput_items,
+    gr_vector_const_void_star &input_items,
+    gr_vector_void_star &output_items) {
+  auto *out = (gr_complex *) output_items[0];
+  if (waveform.empty()) // No waveform specified
+    return -1;
+  for (int i = 0; i < noutput_items; i++) {
+    memcpy(out, waveform.data(), waveform.size() * sizeof(gr_complex));
+    out += waveform.size();
+    if (set_tags) {
+      for (auto &tag : tags) {
+        this->add_item_tag(0,
+            this->nitems_written(0) + i * waveform.size() + tag.offset,
+            tag.value, tag.srcid);
+      }
+    }
+
+  }
+  return noutput_items;
+
+
+  // Tell runtime system how many output items we produced.
+  return noutput_items;
+}
+
+void lfm_source_impl::generateWaveform() {
+  float phase;
+  std::vector<float> t;
+  t = arange(static_cast<float>(0),
+      static_cast<float>(sweep_time),
+      static_cast<float>(1 / samp_rate));
+  // TODO: Change sweep_time back to pri
+  waveform = std::vector<gr_complex>(samp_rate * pri, 0);
+
+  // Create the waveform
+  for (int ii = 0; ii < t.size(); ii++) {
+    phase = -2.0f * M_PI * (bandwidth / 2) * t[ii] + M_PI * bandwidth /
+        sweep_time * std::pow(t[ii], 2);
+    waveform[ii] = std::exp(J * phase);
+  }
+
+}
+
+} /* namespace dragon */
+} /* namespace gr */
+
---
+++ gr-dragon/lib/lfm_source_impl.h
@@ -0,0 +1,81 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2021 gr-dragon author.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_DRAGON_LFM_SOURCE_IMPL_H
+#define INCLUDED_DRAGON_LFM_SOURCE_IMPL_H
+
+#include <dragon/lfm_source.h>
+
+namespace gr {
+namespace dragon {
+
+class lfm_source_impl : public lfm_source {
+ private:
+  /*
+   * Constants
+   */
+  const gr_complex J = gr_complex(0, 1);
+
+  /*
+   * Waveform parameters
+   */
+  float bandwidth;
+  float samp_rate;
+  float sweep_time;
+  float prf;
+  float pri;
+  std::vector<gr_complex> waveform; // Output waveform
+
+  std::vector<tag_t> tags;
+  bool set_tags; // Indicates whether or not tags should be appended
+
+  uint32_t pulse_ctr; // Tracks pulse progression over time
+  /*
+   * Keeps track of how much of the waveform we've transmitted at the start
+   * of work()
+   */
+  uint32_t offset;
+
+  /*
+   * Private functions
+   */
+  void generateWaveform(); // Generates a new LFM waveform
+
+ public:
+  lfm_source_impl(float bandwidth,
+      float sweep_time,
+      float samp_rate,
+      float prf,
+      const std::vector<tag_t>& tags);
+  ~lfm_source_impl() override;
+
+  // Where all the action really happens
+  int work(
+      int noutput_items,
+      gr_vector_const_void_star &input_items,
+      gr_vector_void_star &output_items
+  );
+};
+
+} // namespace dragon
+} // namespace gr
+
+#endif /* INCLUDED_DRAGON_LFM_SOURCE_IMPL_H */
+
