29165fb4008abe1f9deb6f91595a473dfd396cf6
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2021-06-17 10:28:06 -0500

    Added showGraph() function

--- matlab/+runtime/TopBlock.m
+++ matlab/+runtime/TopBlock.m
@@ -4,7 +4,7 @@ classdef TopBlock < handle
   % flowgraph, which is an instance of this class. When a block object is
   % created, its parent is given as this and its name is added to the
   % "blocks" property
-  properties 
+  properties
     blocks runtime.Block
     
   end
@@ -13,7 +13,7 @@ classdef TopBlock < handle
     
     function addBlock(obj,block)
       
-      % Add a block to the list 
+      % Add a block to the list
       if isempty(block)
         obj.blocks = block;
       else
@@ -47,43 +47,66 @@ classdef TopBlock < handle
     end
     
     function showGraph(obj)
-      % TODO: Use graphviz to show the current flowgraph for this block
+      % Use graphviz to show the current flowgraph for this block
       
       % Create graphviz instructions
-      str = ['echo ''digraph {a -> b'];
-      str = [str '}'' | dot -Tpng > output.png'];
-      % Get dot strings, get dot labels, etc
+      command = 'echo ''strict digraph { rankdir=LR node [shape=box]';
+      for iBlock = 1:length(obj.blocks)
+        % Only print the graph path if 
+        if isa(obj.blocks(iBlock),'runtime.SinkBlock')
+          command = [command obj.printGraph(obj.blocks(iBlock))];
+        end
+      end
       
-      % Write to a .gv file
-      
-      % Load .gv file and output to a png
+      command = [command '}'' | dot -Tpng > output.png'];
       
       % Use imshow on the opened png
       
-      system(str)
+      system(command)
       imshow('output.png')
     end
     
-%     function outStr = printGraph(obj)
-%       error('This feature is not yet implemented')
-%     end
-    
-    function out = printGraph(obj)
+    % TODO:
+    % - Decide if this should be static and whether or not the user should be
+    %   able to call it
+    % - Decide if the tempStr parameter should become some form of a persistent
+    %   variable
+    % - Update this function to use a "name" parameter for each block rather
+    %   than just the block name
+    function outStr = printGraph(obj,block,tempStr)
       % Print the graph associated with the input block, with the output given
       % in the dot language. This block will eventually be used to generate a
       % graphviz output of the flowgraph
       
-%       while ~isa(block,'runtime.SourceBlock')
-%         block = block.inputPorts(1).connections(1).parent;
-%       end
-%       
-%       while ~isa(block,'runtime.SinkBlock')
-%         out = [out class(block) ' -> '];
-%         block = block.outputPorts(1).connections(1).parent;
-%       end
-%       out = [out class(block) ';'];
+      outStr = '';
+      name = split(class(block),'.');
+      % If inStr is not specified, this is the first call from an external
+      % function call. The block is then assumed to be a sink block
+      if nargin == 2
+        tempStr = [name{2}];
+      else
+        tempStr = [name{2} ' -> ' tempStr];
+      end
+      
+      % Add a semicolon to the end of every sink block name to mark the end of
+      % the graph path
+      if isa(block,'runtime.SinkBlock')
+        tempStr = [tempStr ';'];
+      end
+      
+      
+      % Append all children graphs to the output
+      for iPort = 1 : length(block.inputPorts)
+        outStr = [obj.printGraph(block.inputPorts(iPort).connections.parent,tempStr) outStr];
+      end
+      
+      % If we've reached a source block, the graph ends 
+      if isa(block,'runtime.SourceBlock')
+        outStr = tempStr;
+      end
+      
     end
-        
+    
     
   end
 end
--- matlab/test/runtime/test_TopBlock.m
+++ matlab/test/runtime/test_TopBlock.m
@@ -13,40 +13,37 @@ cs1.outputPorts(1).connect(eb.inputPorts(1));
 eb.outputPorts(1).connect(ns.inputPorts(1));
 
 % Test that the function works properly regardless of which block is used for the call 
-expected = 'blocks.ConstantSource -> blocks.EmptyBlock -> blocks.NullSink;';
+expected = 'ConstantSource -> EmptyBlock -> NullSink;';
 
 actual = tb.printGraph(ns);
 testCase.verifyEqual(actual,expected)
 
-actual = tb.printGraph(eb);
-testCase.verifyEqual(actual,expected)
-
-actual = tb.printGraph(cs1);
-testCase.verifyEqual(actual,expected)
+% actual = tb.printGraph(eb);
+% testCase.verifyEqual(actual,expected)
+% 
+% actual = tb.printGraph(cs1);
+% testCase.verifyEqual(actual,expected)
 
 end
 
 function test_printGraph_connected_MultiInput(testCase)
+
 tb = runtime.TopBlock();
 cs1 = blocks.ConstantSource(tb,1);
 cs2 = blocks.ConstantSource(tb,2);
-eb = blocks.EmptyBlock(tb);
-eb.addInputPort();
+
+eb1 = blocks.EmptyBlock(tb);
+eb1.addInputPort();
 ns = blocks.NullSink(tb);
-cs1.outputPorts(1).connect(eb.inputPorts(1));
-cs2.outputPorts(1).connect(eb.inputPorts(2));
-eb.outputPorts(1).connect(ns.inputPorts(1));
+
+cs1.outputPorts(1).connect(eb1.inputPorts(1));
+cs2.outputPorts(1).connect(eb1.inputPorts(2));
+eb1.outputPorts(1).connect(ns.inputPorts(1));
+
 
 % Test that the function works properly regardless of which block is used for the call 
-expected = '{blocks.ConstantSource blocks.ConstantSource} -> {blocks.EmptyBlock} -> blocks.NullSink;';
+expected = 'ConstantSource -> EmptyBlock -> NullSink;ConstantSource -> EmptyBlock -> NullSink;';
 
-actual = tb.printGraph();
+actual = tb.printGraph(ns);
 testCase.verifyEqual(actual,expected)
-% 
-% actual = tb.printGraph();
-% testCase.verifyEqual(actual,expected)
-% 
-% actual = tb.printGraph();
-% testCase.verifyEqual(actual,expected)
-
 end
