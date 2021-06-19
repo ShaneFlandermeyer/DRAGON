1ac984a77c6723feb4d67826261ff6862f320006
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2021-06-19 07:57:40 -0500

    Changed port and queue permissions from protected to private

--- matlab/+runtime/OutputPort.m
+++ matlab/+runtime/OutputPort.m
@@ -8,19 +8,14 @@ classdef OutputPort < runtime.Port
     
     function connect(obj,port)
       validateattributes(port,'runtime.InputPort',{})
-      obj.connections = [obj.connections; port];
-      port.connections = obj;
+      obj.addConnection(port);
+      port.addConnection(obj);
     end
     
     function disconnect(obj,port)
       validateattributes(port,'runtime.InputPort',{})
-      % Find the connection in this object's list and delete it
-      idx = find(obj.connections == port);
-      obj.connections(idx) = [];
-      
-      % Find the connection in the input port's list and delete it
-      idx = find(port.connections == obj);
-      port.connections(idx) = [];
+      obj.deleteConnection(port);
+      port.deleteConnection(obj);
     end
     
   end
